5fa38053035c6fbe464a04a6bc2338d197a993a5
bst <bst@BST-PC>
2018-08-08 17:22:54 +0800

    optimization set thread Name

--- MyWinThread/MyWinThread.cpp
+++ MyWinThread/MyWinThread.cpp
@@ -1,5 +1,9 @@
 #include "MyWinThread.h"
 
+/**
+*   作用：
+*       全局函数，做测试用，无实际作用（可删除）。
+*/
 int myPrintfVal(int a)
 {
     int its_a = a;
@@ -29,36 +33,10 @@ void CMyWinTthread::threadProc(void * pVoid)
     }
 }
 
-//void CMyWinTthread::threadProc2(void * pVoid)
-//{
-//    if (NULL != pVoid)
-//    {
-//        ((CMyWinTthread*)pVoid)->threadFunc2();
-//    }
-//}
-//
-//void CMyWinTthread::threadProc3(void * pVoid)
-//{
-//    if (NULL != pVoid)
-//    {
-//        ((CMyWinTthread*)pVoid)->threadFunc3();
-//    }
-//}
-
 void CMyWinTthread::startThread()
 {
     m_threadHandle = (HANDLE)_beginthread(threadProc, 0, (void*)this);
 }
-
-//void CMyWinTthread::startThread2()
-//{
-//    m_threadHandle2 = (HANDLE)_beginthread(threadProc2, 0, (void*)this);
-//}
-//
-//void CMyWinTthread::startThread3()
-//{
-//    m_threadHandle3 = (HANDLE)_beginthread(threadProc3, 0, (void*)this);
-//}
 
 void CMyWinTthread::threadFunc()
 {
@@ -75,24 +53,6 @@ void CMyWinTthread::threadFunc()
     }
 }
 
-//void CMyWinTthread::threadFunc2()
-//{
-//    while (m_bRuning)
-//    {
-//        printf("in thread2   m_value = %d\n", m_value);
-//        Sleep(2 * 1000);
-//    }
-//}
-//
-//void CMyWinTthread::threadFunc3()
-//{
-//    while (m_bRuning)
-//    {
-//        printf("in thread3   m_value = %d\n", m_value);
-//        Sleep(3 * 1000);
-//    }
-//}
-
 void CMyWinTthread::setValue(int i)
 {
     m_value = i;
@@ -102,16 +62,13 @@ void CMyWinTthread::start()
 {
     m_bRuning = true;
 
-    startThread();
+    m_threadHandle = (HANDLE)_beginthread(threadProc, 0, (void*)this);
 
     m_ExitEvent = CreateEvent(
         NULL,    // default security attributes
         TRUE,    // manual reset event
         FALSE,   // not signaled
         NULL);   // no name
-
-    //startThread2();
-    //startThread3();
 }
 
 void CMyWinTthread::stop()
@@ -127,46 +84,17 @@ void CMyWinTthread::join()
 
 void CMyWinTthread::SetThreadName(const char* threadName)
 {
-    m_threadInfo.dwType = 0x1000;
-    m_threadInfo.szName = threadName;
-    m_threadInfo.dwThreadID = m_threadId;
-    m_threadInfo.dwFlags = 0;
+    THREADNAME_INFO threadInfo;   //保存线程名的结构体
+    threadInfo.dwType = 0x1000;
+    threadInfo.szName = threadName;
+    threadInfo.dwThreadID = m_threadId;
+    threadInfo.dwFlags = 0;
 
     __try 
     {
-        RaiseException(MS_VC_EXCEPTION, 0, sizeof(m_threadInfo) / sizeof(ULONG_PTR), (ULONG_PTR*)&m_threadInfo);
+        RaiseException(MS_VC_EXCEPTION, 0, sizeof(threadInfo) / sizeof(ULONG_PTR), (ULONG_PTR*)&threadInfo);
     }
     __except (EXCEPTION_EXECUTE_HANDLER) 
     {
     }
 }
-
-//  
-// Usage: SetThreadName ((DWORD)-1, "MainThread");  
-//  
-//#include <windows.h>  
-//const DWORD MS_VC_EXCEPTION = 0x406D1388;
-//#pragma pack(push,8)  
-//typedef struct tagTHREADNAME_INFO
-//{
-//    DWORD dwType; // Must be 0x1000.  
-//    LPCSTR szName; // Pointer to name (in user addr space).  
-//    DWORD dwThreadID; // Thread ID (-1=caller thread).  
-//    DWORD dwFlags; // Reserved for future use, must be zero.  
-//} THREADNAME_INFO;
-//#pragma pack(pop)  
-//void SetThreadName(DWORD dwThreadID, const char* threadName) {
-//    THREADNAME_INFO info;
-//    info.dwType = 0x1000;
-//    info.szName = threadName;
-//    info.dwThreadID = dwThreadID;
-//    info.dwFlags = 0;
-//#pragma warning(push)  
-//#pragma warning(disable: 6320 6322)  
-//    __try {
-//        RaiseException(MS_VC_EXCEPTION, 0, sizeof(info) / sizeof(ULONG_PTR), (ULONG_PTR*)&info);
-//    }
-//    __except (EXCEPTION_EXECUTE_HANDLER) {
-//    }
-//#pragma warning(pop)  
-//}
--- MyWinThread/MyWinThread.h
+++ MyWinThread/MyWinThread.h
@@ -11,14 +11,17 @@ const DWORD MS_VC_EXCEPTION = 0x406D1388;
 
 typedef struct tagTHREADNAME_INFO
 {
-    DWORD dwType; // Must be 0x1000.  
-    LPCSTR szName; // Pointer to name (in user addr space).  
-    DWORD dwThreadID; // Thread ID (-1=caller thread).  
-    DWORD dwFlags; // Reserved for future use, must be zero.  
+    DWORD dwType;       // Must be 0x1000.  
+    LPCSTR szName;      // Pointer to name (in user addr space).  
+    DWORD dwThreadID;   // Thread ID (-1=caller thread).  
+    DWORD dwFlags;      // Reserved for future use, must be zero.  
 } THREADNAME_INFO;
 
 
-
+/**
+*   功能：
+*       把线程封装成类，方便后续使用，创建线程时需要指定线程名称，如果不指定，则使用默认名称 "myThread".
+*/
 class CMyWinTthread
 {
 public:
@@ -30,37 +33,28 @@ public:
     void join();
 
     static void threadProc(void * pVoid);
-    //static void threadProc2(void * pVoid);
-    //static void threadProc3(void * pVoid);
-
     void startThread();
-    //void startThread2();
-    //void startThread3();
-
     void threadFunc();
-    //void threadFunc2();
-    //void threadFunc3();
-
     void setValue(int i);
+
+    /**
+    *   作用：
+    *       设置线程的名称，调试时使用；日常代码中使用成员变量 'm_threadName' 即可。
+    */
     void SetThreadName(const char* threadName);
 
 private:
     int m_value;
     bool m_bRuning;
     char m_threadName[40];
-    THREADNAME_INFO m_threadInfo;   //保存线程名的结构体
     DWORD m_threadId;               //线程ID
-
-    HANDLE m_threadHandle;
+    HANDLE m_threadHandle;          //线程句柄
 
     /**
     *   变量名：m_ExitEvent
     *   作用：线程函数执行完成正常结束，或者收到该事件则立即结束; 通过控制该事件状态来强制结束线程
     */
     HANDLE m_ExitEvent; 
-
-    //HANDLE m_threadHandle2;
-    //HANDLE m_threadHandle3;
 };
 
 
