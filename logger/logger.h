a57b2b11d96d8619bc9182f7eeb7830df36f3db7
bst <bst@BST-PC>
2018-10-23 15:45:08 +0800

    formate logger print

--- logger/logger.h
+++ logger/logger.h
@@ -7,7 +7,7 @@
 #ifdef NODEBUG
 #define LOG(fmt, ...) do {} while (0)
 #else
-#define LOG(fmt, ...) fprintf(stdout, "[DEBUG] %s:%s:%d: " fmt "\n", __FILE__, __FUNCTION__, __LINE__, ##__VA_ARGS__)
+#define LOG(fmt, ...) fprintf(stdout, "[DEBUG] %s\n%s:%d:" fmt "\n", __FILE__, __FUNCTION__, __LINE__, ##__VA_ARGS__)
 #endif
 #endif // !__LOGGER_H__
 
