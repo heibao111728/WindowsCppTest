7246136fc76591139497d1ba86ee6d7d552c7f80
yangdong-MatebookXPro <heibao111728@126.com>
2019-04-17 21:25:27 +0800

    tmp

--- WindowsCppTest/main.cpp
+++ WindowsCppTest/main.cpp
@@ -2529,32 +2529,49 @@ int main()
 }
 #endif
 
-#if 1
+/**
+    c++ map±éÀúÉ¾³ı
+*/
+#if 0
 
 #include<stdio.h>
+#include <iostream>
+#include <map>
+
+using namespace std;
+
 int main()
 {
-    int a[60];
-    int b[60];
-    int p1 = 0, p2 = 0;
-    int i, j;
-    for (i = 2;i <= 60;i++)
+    map<int, char*> tmp_map;
+    tmp_map[0] = "yang";
+    tmp_map[1] = "dong";
+    tmp_map[2] = "shi";
+    tmp_map[3] = "ge";
+    tmp_map[4] = "hao";
+    tmp_map[5] = "nan";
+    tmp_map[6] = "ren";
+
+    for (map<int, char*>::iterator it = tmp_map.begin(); it != tmp_map.end(); /*it++*/)
     {
-        int sum = 0;
-        for (j = 1;j<i;j++)
-        {
-            if (i % j == 0)sum += j;
-        }
-        if (sum == i)
-            a[p1++] = i;
-        else if (sum > i)
-            b[p2++] = i;
+        //it = tmp_map.erase(it);
+        tmp_map.erase(it++);
     }
-    printf("E: ");
-    for (i = 0;i<p1;i++)
-        printf("%d ", a[i]);
-    printf("G: 2 ");
-    for (i = 0;i<p2;i++)
-        printf("%d ", b[i]);
+
+    return 0;
 }
+#endif
+
+#if 1
+
+#include "stdio.h"
+
+int main()
+{
+    int * pdata;
+
+    printf("%d\n", sizeof(short*));
+
+    return 0;
+}
+
 #endif
